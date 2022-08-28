45ab78e6e13c8b3c51806dbeebc09e6f37696fe0
karolinathorwarth@gmail.com <karolinathorwarth@web.de>
2022-08-28 22:27:35 +0200

    btn-fuctionality:className, useState, Hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
+// after coloring buttons , add functionality onto the external div with className "react-root": by adding the class red to the red button, "blue" to the blue button etc if I click the  red/ blue/ green button => so I use here STATE ( => state isany data that changes over the course of the applicaation running) & REACT HOOKS ( write components using functions (instead of needing to use classes or objects)
+// HOOKS are functions that  allow to hook into react features (added on top of app component)
+
 function App() {
+  // use array destructuring=>  store these two array items in different variables( color, setColor in const and in clg)
+  const [color, setColor] = useState("yellow");
+  // consoloe .log to see , what "color" is=> it's an array with 2 items in it: the first one is undefined and the second is a function
+  // so the first item is the current value in state, so whatever color is set to at the moment and then the function is going to allow us to update state and set the color variable to different color
+  console.log(color, setColor);
   return (
     <div className="react-root">
       {/* center the content on the page */}
