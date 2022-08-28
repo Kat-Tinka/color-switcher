7a6991f85142086e061e5dbe9a6a2ee4d6e66e0c
karolinathorwarth@gmail.com <karolinathorwarth@web.de>
2022-08-28 21:49:46 +0200

    create buttons with classNames

--- src/App.js
+++ src/App.js
@@ -2,7 +2,19 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="react-root">
+      {/* center the content on the page */}
+      <div className="centered">
+        {/* header to title the page */}
+        <h1>Color Picker</h1>
+        {/* three buttons with classNames */}
+        <button className="red">red</button>
+        <button className="blue">blue</button>
+        <button className="green">green</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
