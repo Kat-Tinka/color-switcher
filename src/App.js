4c557396cfe7fb46f5e2cc10f17afc50e97fd58d
karolinathorwarth@gmail.com <karolinathorwarth@web.de>
2022-08-28 23:41:49 +0200

    comments deleted for better overview

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React, { useState } from "react";
 import ColorChangeButton from "./colorchangebutton";
 import "./App.css";
 
-// import created colorChangebtn-file and add  the button*3 after <h1>
-// replace the oroginal btns with the new ones-> so  their colors and the setColor functionality should work here=> by using PROPS =< PROPS is data that is passed from component to the component in react (=> color="red"/ etc.)
-// when I check now the props in the console=> I see the color for each button and also the setColor function
 function App() {
   const [color, setColor] = useState("");
   // console.log(color, setColor);
@@ -16,7 +13,6 @@ function App() {
         <ColorChangeButton color="blue" setColor={setColor} />
         <ColorChangeButton color="green" setColor={setColor} />
       </div>
-      {/* the "old buttons " are deleted now */}
     </div>
   );
 }
--- src/colorchangebutton.js
+++ src/colorchangebutton.js
@@ -1,13 +1,6 @@
 import React from "react";
 
-// create a function
 function ColorChangeButton(props) {
-  // in console you see the colors red, blue, green  => all are an object
-  //   console.log(props);
-  //   add the color-key => props.color in {} (to say, this is jsx-code)
-  //   & add the className of that props.color
-
-  // now pass the setColor-function(with eventListener) from the App.js-file, using props
   return (
     <button className={props.color} onClick={() => props.setColor(props.color)}>
       {props.color}
